bad68409a68d46d61a4fc47ad5e230fc0bc3bb5a
841723 <841723@unizar.es>
2024-06-20 13:54:32 +0200

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://841723.github.io',
-  base: '/portfolio/',
+  // base: '/portfolio/',
   integrations: [tailwind()],
 });
