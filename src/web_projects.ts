c2c3d0ffff6ecd9466557e44ccd0ff5a489407b9
diego.uruena <118764694+841723@users.noreply.github.com>
2024-11-15 18:14:42 +0100

    Update web_projects.ts

--- src/web_projects.ts
+++ src/web_projects.ts
@@ -4,10 +4,21 @@ import { BASE_GITHUB_URL } from "./consts";
 export const WEB_PROJECTS = [
     {
         title: "LOCATOPEDIA",
-        description: "",
+        subtitle: "Currently in progress...",
+        description:
+            "Wiki-like website to search for locations and their information. Add new locations and edit them. Check the map to see the locations. Create an account to save your favorite locations.",
         img: "/tfg/logo.jpg",
-        used_tech: [TAGS.React, TAGS.NodeJs, TAGS.PostgreSQL, TAGS.TailwindCSS, TAGS.Docker, TAGS.JavaScript, TAGS.Nginx],
-    }
+        // gh_link: BASE_GITHUB_URL + "wikiplace",
+        used_tech: [
+            TAGS.React,
+            TAGS.NodeJs,
+            TAGS.PostgreSQL,
+            TAGS.TailwindCSS,
+            TAGS.Docker,
+            TAGS.JavaScript,
+            TAGS.Nginx,
+        ],
+    },
     {
         title: "REFORMAS LAS CANTERAS",
         subtitle: "Currently in progress...",
@@ -21,7 +32,7 @@ export const WEB_PROJECTS = [
         description:
             "Online game to play Cluedo with bots or your friends. Chat with them, find the killer, the weapon, and the room, and win the game.",
         img: "/asesin-ada/logo.png",
-        gh_link: BASE_GITHUB_URL+"asesin-ada",
+        gh_link: BASE_GITHUB_URL + "asesin-ada",
         used_tech: [
             TAGS.React,
             TAGS.NodeJs,
@@ -36,14 +47,20 @@ export const WEB_PROJECTS = [
         description:
             "Ecommerce website inspired by PC Componentes. Search for products, filter, sort and add them to your cart. Purchase the ones you like.",
         img: "/pc-arp/logo_square.png",
-        gh_link: BASE_GITHUB_URL+"pcarp",
-        used_tech: [TAGS.HTML, TAGS.CSS, TAGS.JavaScript, TAGS.NodeJs, TAGS.PostgreSQL],
+        gh_link: BASE_GITHUB_URL + "pcarp",
+        used_tech: [
+            TAGS.HTML,
+            TAGS.CSS,
+            TAGS.JavaScript,
+            TAGS.NodeJs,
+            TAGS.PostgreSQL,
+        ],
     },
     {
         title: "PORTFOLIO",
         description: "This portfolio you are seeing right now.",
         img: "/portfolio/logo.png",
-        gh_link: BASE_GITHUB_URL+"portfolio",
+        gh_link: BASE_GITHUB_URL + "portfolio",
         preview_link: "https://841723.github.io/portfolio/",
         used_tech: [TAGS.Astro, TAGS.Typescript, TAGS.TailwindCSS],
     },
